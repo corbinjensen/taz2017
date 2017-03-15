b493b51699bd220ceef1c1d44c55bd4799473fa3
Corbin Jensen <corbjens@gmail.com>
2017-03-14 19:39:53 -0600

    Updated footer logo, reponsive edits, new B

--- tazMen2017/src/App.js
+++ tazMen2017/src/App.js
@@ -2,22 +2,21 @@ import React, { Component } from 'react';
 import Header from './cmps/Header'
 import Registration from './cmps/Registration'
 import Documents from './cmps/Documents.js'
-import Tazopia from './cmps/Tazopia'
-import logo from './logo.svg';
+import Footer from './cmps/Footer'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+
         <Header />
 
         <Registration />
 
         <Documents />
 
-        <Tazopia />
+        <Footer />
 
       </div>
     );
---
+++ tazMen2017/src/cmps/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Tazopia from './Tazopia'
+
+class Footer extends React.Component {
+  render(){
+    return (
+      <footer>
+        <Tazopia />
+      </footer>
+    )
+  }
+}
+
+export default Footer
